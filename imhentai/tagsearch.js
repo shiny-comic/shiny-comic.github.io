d5835a34672801fab8f9091a9c178536dece48c6
rasp <ny@raspi>
2023-03-08 11:12:04 +0900

    fix url

--- imhentai/tagsearch.js
+++ imhentai/tagsearch.js
@@ -1,6 +1,6 @@
 function replacetag(){
   var tags = document.querySelectorAll('a.tag');
-  var prefix = 'https://imhentai.xxx/advsearch/?lt=0&pp=0&w=0&i=0&es=0&fr=0&kr=0&de=0&ru=0&dl=0&tr=0&key=%2B';
+  var prefix = 'https://imhentai.xxx/advsearch/?lt=0&apply=Search&pp=0&w=0&i=0&en=0&es=0&fr=0&kr=0&de=0&ru=0&dl=0&tr=0&key=%2B';
   for (let tag of tags) {
     var href = tag.href.split('/').slice(-3, -1);
     if (tag.querySelector('.split_tag')) {
