6c4950ef1f9f39915b30f0c6a8a608f85ae8c2bd
rasp <ny@raspi>
2023-03-09 03:19:33 +0900

    update bookmark

--- bookmarklets/zipimage.js
+++ bookmarklets/zipimage.js
@@ -6,13 +6,15 @@ function zipit() {
   } else {
     if(a[3] == 'gallery') {
       var galleryId = a.slice(-2)[0];
+      var pages = document.querySelector('.pages').innerText.split(' ')[1];
     } else if(a[3] == 'view') {
       var galleryId = a[4];
+      var pages = a[5];
     } else {
       alert('Invalid URL: '+document.location.href);
       return 1;
     }
-    var range = prompt('Input range of images e.g. 1-32,34');
+    var range = prompt('Input range of images e.g. 1-32,34', '1-'+pages);
     window.open('http://pi.hole/test/zip-images.html?id='+galleryId+'&range='+range, '');
   }
 };
